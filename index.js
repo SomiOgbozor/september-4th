040f8478d03a7b6384e7e5d5e0c4f416a4e000a7
SomiOgbozor <somiogbozor@wustl.edu>
2020-09-01 19:29:34 -0500

    added magic css implementation

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function (){
+	// const selector = document.querySelector('.header');
+	// selector.classList.add('magictime', 'foolishIn');
+	const vidSelector = document.querySelector('.video-container');
+	vidSelector.classList.add('magictime', 'foolishIn');
+}
+// setTimeout(delayVid, 3000);
