60cfaf37002d650299935cdb92c861f2f11debc6
SomiOgbozor <somiogbozor@wustl.edu>
2020-09-02 10:25:26 -0500

    testing mobile layout with swipe.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-window.onload = function (){
-	// const selector = document.querySelector('.header');
-	// selector.classList.add('magictime', 'foolishIn');
-	const vidSelector = document.querySelector('.video-container');
-	vidSelector.classList.add('magictime', 'foolishIn');
-}
-// setTimeout(delayVid, 3000);
+var swiper = new Swiper('.swiper-container');
+// window.onload = function (){
+// 	// const selector = document.querySelector('.header');
+// 	// selector.classList.add('magictime', 'foolishIn');
+// 	const vidSelector = document.querySelector('.video-container');
+// 	vidSelector.classList.add('magictime', 'foolishIn');
+// }
+// // setTimeout(delayVid, 3000);
